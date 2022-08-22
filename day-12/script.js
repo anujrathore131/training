1be4e8fc4ff00a1facaa228cadb8c06548604ece
anujrathore131 <anujrathore131@gmail.com>
2022-08-22 18:01:24 +0530

    day-12

--- day-12/script.js
+++ day-12/script.js
@@ -1,9 +1,40 @@
 var inp=document.querySelector('.todo-input');
 var btn=document.querySelector('button');
 var list=document.querySelector('.todo-list');
+var out=window.localStorage.getItem("data");
+var d=[];
+var i=0;
+if(out==""||out==null)
+{
+    alert("No Work To Be Done");
+}
+else
+{
+d=out.split(',');
+while(i<d.length){
+    var newdiv=document.createElement("div");
+    newdiv.classList.add('tododiv');
+    var newli=document.createElement("li");
+    newli.classList.add('tododiv_item');
+    newli.innerHTML=d[i];
+    newdiv.appendChild(newli);
+    var checkbtn=document.createElement("button");
+    checkbtn.classList.add('tododiv_check');
+    checkbtn.innerHTML='<i class="fa fa-check"></i>';
+    newdiv.appendChild(checkbtn);
+    var endbtn=document.createElement("button");
+    endbtn.classList.add('tododiv_end');
+    endbtn.innerHTML='<i class="fa fa-trash"></i>';
+    newdiv.appendChild(endbtn);
+    list.appendChild(newdiv);
+    i++;
+};
+}
 btn.onclick = function crtodo(e){
     e.preventDefault();
     if(inp.value.length>0){
+    d.push(inp.value);
+    window.localStorage.setItem("data",d);
     var newdiv=document.createElement("div");
     newdiv.classList.add('tododiv');
 
@@ -11,12 +42,10 @@ btn.onclick = function crtodo(e){
     newli.classList.add('tododiv_item');
     newli.innerHTML=inp.value;
     newdiv.appendChild(newli);
-
     var checkbtn=document.createElement("button");
     checkbtn.classList.add('tododiv_check');
     checkbtn.innerHTML='<i class="fa fa-check"></i>';
     newdiv.appendChild(checkbtn);
-
     var endbtn=document.createElement("button");
     endbtn.classList.add('tododiv_end');
     endbtn.innerHTML='<i class="fa fa-trash"></i>';
@@ -31,6 +60,9 @@ list.onclick =function checkbtn(e){
         if(check.classList[0]=="tododiv_end")
         {
             var parentnode=check.parentElement;
+            var data=d.indexOf(parentnode.innerText);
+            d.splice(data,1);
+            window.localStorage.setItem("data",d);
             parentnode.remove();
         }
         if(check.classList[0]=="tododiv_check")
